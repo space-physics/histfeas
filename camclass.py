fb74f519c7d24bfef06f95747129f40957eb04d5
scivision <scivision@users.noreply.github.com>
2015-02-24 00:30:47 -0500

    initial commit

---
+++ camclass.py
@@ -0,0 +1,302 @@
+from __future__ import print_function, division
+from numpy import (linspace, fliplr, flipud, rot90, arange,
+                   polyfit,polyval,rint,empty, isfinite, 
+                   absolute, hypot, logical_or, unravel_index, delete, where)
+from os.path import expanduser
+from dateutil.parser import parse
+from scipy.signal import savgol_filter
+from six import string_types
+from numpy.random import standard_normal
+#
+from azel2radec import azel2radec
+from haversine import angledist
+from coordconv3d import aer2ecef
+
+
+class Cam: #use this like an advanced version of Matlab struct
+    def __init__(self,sim,cp,name,zmax,dbglvl):
+        self.dbglvl = dbglvl
+        self.name = name
+        self.lat = cp['latWGS84']
+        self.lon = cp['lonWGS84']
+        self.alt_m = cp['Zkm']*1e3
+        self.z_km = cp['Zkm']
+        self.x_km = cp['Xkm']
+        self.fn = cp['fn']
+#        self.startTime = startTime
+#        self.stopTime = stopTime
+        self.nCutPix = int(cp['nCutPix'])
+        self.xpix = cp['xPix']
+        self.ypix = cp['yPix']
+        self.xbin = cp['xBin']
+        self.ybin = cp['yBin']
+        self.Bincl = cp['Bincl']
+        self.Bdecl = cp['Bdecl']
+        self.Bepoch = cp['Bepoch'] #it's OK, I want to feedthru nan if xls cell is empty!
+
+        if isinstance(self.Bepoch,string_types):
+            self.Bepoch = parse(self.Bepoch)
+
+        if isfinite(self.Bincl) and isfinite(self.Bdecl):
+            self.Baz = 180. + self.Bdecl
+            self.Bel = self.Bincl
+        else:
+            self.Baz = None; self.Bel = None
+
+        self.rotCCW =    cp['rotCCW']
+        self.transpose = cp['transpose'] == 1
+        self.flipLR =    cp['flipLR'] == 1
+        self.flipUD =   cp['flipUD'] == 1
+        self.timeShiftSec = cp['timeShiftSec']
+
+        self.plotminmax = (cp['plotMinVal'], cp['plotMaxVal'])
+        self.fullstart = cp['fullFileStartUTC']
+
+        self.intensityScaleFactor = cp['intensityScaleFactor']
+        self.lowerthres = cp['lowerthres']
+
+#%% check FOV and 1D cut sizes for sanity
+        self.fovmaxlen = cp['FOVmaxLengthKM']
+
+        if self.fovmaxlen > 10e3:
+            print('sanityCheck: Your FOV length seems excessive > 10000 km')
+        if self.nCutPix > 4096:
+            print('sanityCheck: Program execution time may be excessive due to large number of camera pixels')
+        if self.fovmaxlen < (1.5*zmax):
+            print('sanityCheck: To avoid unexpected pixel/sky voxel intersection problems, make your candidate camera FOV at least 1.5 times longer than your maximum Z altitude.')
+
+        self.boresightEl = cp['boresightElevDeg']
+        self.arbfov = cp['FOVdeg']
+#%%
+        self.kineticSec = 1. / cp['frameRateHz']
+#%% sky mapping
+        cal1Ddir = sim.cal1dpath
+        cal1Dname = cp['cal1Dname']
+        if isinstance(cal1Ddir,string_types) and isinstance(cal1Dname,string_types):
+            self.cal1Dfn = expanduser(cal1Ddir + cal1Dname)
+            
+        self.raymap = sim.raymap
+        if self.raymap == 'arbitrary':
+            self.angle_deg = self.arbanglemap()
+#        elif self.raymap == 'astrometry': #not OOPable at this time
+#            self.angle_deg = self.astrometrymap()
+#        else:
+#            exit('*** unknown ray mapping method ' + self.raymap)
+#%% pixel noise/bias
+        self.noiseStd = cp['noiseStd']
+        self.ccdbias = cp['CCDBias']
+        self.debiasData = cp['debiasData']
+        self.smoothspan = cp['smoothspan']
+        self.savgolOrder = cp['savgolOrder']
+
+
+    def ingestcamparam(self,finf):
+        self.SuperX=finf['superx']
+        self.SuperY=finf['supery']
+        self.Nmetadata = finf['nmetadata']
+        self.BytesPerFrame= finf['bytesperframe']
+        self.PixelsPerImage = finf['pixelsperimage']
+
+    def arbanglemap(self):
+        '''
+        here's the center angle of each pixel ( + then - to go from biggest to
+        smallest angle)  (e.g. 94.5 deg to 85.5 deg. -- sweeping left to right)
+        '''
+        #raySpacingDeg = self.arbfov / self.nCutPix
+        maxAng = self.boresightEl + self.arbfov/2
+        minAng = self.boresightEl - self.arbfov/2
+        return linspace(maxAng, minAng, num=self.nCutPix, endpoint=True)
+
+    def astrometrymap(self):
+        pass
+        # not sure if get1Dcut.py can be OOP'd
+        #FOVangWidthDeg =pixAngleDeg[-1,iCam] - pixAngleDeg[0,iCam]
+        #ModelRaySpacingDeg = np.mean( np.diff(pixAngleDeg[:,iCam],n=1) ) # for reference purposes
+
+    def toecef(self,ranges):
+        self.x2mz, self.y2mz, self.z2mz = aer2ecef(self.Baz,self.Bel,ranges,
+                                                   self.lat,self.lon,self.alt_m)
+    def doorient(self,az,el,ra,dec):
+        if self.transpose:
+            if self.dbglvl>0:
+                print('tranposing cam ' + self.name + ' az/el/ra/dec data')
+            az  = az.T
+            el  = el.T
+            ra  = ra.T
+            dec = dec.T
+        if self.flipLR:
+            if self.dbglvl>0:
+                print('flipping horizontally cam ' + self.name + ' az/el/ra/dec data')
+            az  = fliplr(az)
+            el  = fliplr(el)
+            ra  = fliplr(ra)
+            dec = fliplr(dec)
+        if self.flipUD:
+            if self.dbglvl>0:
+                print('flipping vertically cam ' + self.name + ' az/el/ra/dec data')
+            az  = flipud(az)
+            el  = flipud(el)
+            ra  = flipud(ra)
+            dec = flipud(dec)
+        if self.rotCCW != 0:
+            if self.dbglvl>0:
+                print('rotating cam ' + self.name + ' az/el/ra/dec data')
+            az  = rot90(az, k = self.rotCCW)
+            el  = rot90(el, k = self.rotCCW)
+            ra  = rot90(ra, k = self.rotCCW)
+            dec = rot90(dec,k = self.rotCCW)
+        self.az = az
+        self.el = el
+        self.ra = ra
+        self.dec = dec
+
+    def debias(self,data):
+        if isfinite(self.debiasData):
+            if self.dbglvl>0:
+                print('Debiasing Data for Camera #' + self.name )
+            data -= self.debiasData
+        return data
+
+    def donoise(self,data):
+         noisy = data.copy()
+
+         if isfinite(self.noiseStd):
+             print('adding noise with std dev {:0.1e}'.format(self.noiseStd) + ' to camera'+self.name)
+             dnoise = self.noiseStd*(standard_normal(size=self.nCutPix))
+             noisy += dnoise
+         else:
+             dnoise = None
+
+
+         if isfinite(self.ccdbias):
+             print('adding bias {:0.1e}'.format(self.ccdbias) + ' to camera'+self.name)
+             noisy += self.ccdbias
+
+         # kept for diagnostic purposes
+         self.raw = data #these are untouched pixel intensities
+         self.dnoise = dnoise
+         self.noisy = noisy
+
+         return noisy
+         """
+         note the method below actually doesn't work quite right--thin spike in real data gets
+         less noise than spread out low peak data!
+         """
+         # Add noise, as percent of signal on PER CAMERA basis
+         #  where is reference for this!?
+         # self.std = self.noiseStd * norm(data,ord=2)
+         #print(norm(data,ord=2))
+         #en = standard_normal(size = self.nCutPix) # vector of zero-mean gaussian noise
+         #return data + self.ccdbias + self.std * en / norm(en,ord=2) #scaling noise
+
+    def dosmooth(self,data):
+        if self.smoothspan > 0 and self.savgolOrder>0:
+            if self.dbglvl>0:
+                print('Smoothing Data for Camera #' + self.name)
+            data= savgol_filter(data, self.smoothspan, self.savgolOrder)
+        return data #LEAVE THIS AS SEPARATE LINE!
+
+    def fixnegval(self,data):
+        negDataInd = data<0
+        if negDataInd.any():
+            print('* Setting',negDataInd.sum(),' negative Data values to 0 for Camera #' + self.name )
+            data[negDataInd] = 0
+
+        self.nonneg = data
+        return data
+    def scaleintens(self,data):
+        if isfinite(self.intensityScaleFactor) and self.intensityScaleFactor !=1 :
+            if self.dbglvl>0:
+                print( 'Scaling data to Cam #' + self.name + ' by factor of ',self.intensityScaleFactor )
+            data *= self.intensityScaleFactor
+            #assert isnan(data).any() == False
+        return data
+    def dolowerthres(self,data):
+        if isfinite(self.lowerthres):
+            print('Thresholding Data to 0 when DN <',self.lowerthres, 'for Camera #' + self.name )
+            data[ data < self.lowerthres ] = 0
+        return data
+
+    def findLSQ(self,nearrow,nearcol):
+        polycoeff = polyfit(nearcol,nearrow,deg=1,full=False)
+        #columns (x)  to cut from picture
+        cutcol = arange(self.xpix,dtype=int) #not range
+        #rows (y) to cut from picture
+        cutrow = rint(polyval(polycoeff,cutcol)).astype(int)
+        assert (cutrow>=0).all() and (cutrow<self.ypix).all()
+
+        #angle from magnetic zenith corresponding to those pixels
+        rapix =  self.ra[cutrow, cutcol]
+        decpix = self.dec[cutrow, cutcol]
+        raMagzen,decMagzen = azel2radec(self.Baz,self.Bel,self.lat,self.lon,self.Bepoch)
+        if self.dbglvl>0: print('mag. zen. ra/dec ' + str((raMagzen,decMagzen)))
+
+        angledist_deg = angledist(raMagzen,decMagzen,rapix,decpix)
+        # put distances into a 90-degree fan beam
+        angle_deg = empty(self.xpix,dtype=float)
+        MagZenInd = angledist_deg.argmin() # whether slightly positive or slightly negative, this should be OK
+
+        angle_deg[MagZenInd:] = 90. + angledist_deg[MagZenInd:]
+        angle_deg[:MagZenInd] = 90. - angledist_deg[:MagZenInd]
+
+        self.angle_deg = angle_deg
+        self.angleMagzenind = MagZenInd
+        self.cutrow = cutrow
+        self.cutcol = cutcol
+
+        if self.dbglvl>0:
+            from matplotlib.pyplot import figure
+            clr = ['b','r','g','m']
+            ax=figure().gca()
+            ax.plot(cutcol,cutrow,color=clr[self.name],
+                    label=self.name, linestyle='-')
+            ax.legend()
+            ax.set_xlabel('x'); ax.set_ylabel('y')
+            ax.set_title('polyfit with computed ray points')
+
+            ax =figure().gca()
+            ax.plot(angle_deg,color=clr[self.name],label='cam'+self.name,
+                    linestyle='None',marker='.')
+            ax.legend()
+            ax.set_xlabel('x-pixel'); ax.set_ylabel('$\theta$ [deg.]')
+            ax.set_title('angle from magnetic zenith $\theta$')
+                                               
+    def findClosestAzel(self,discardEdgepix):
+        assert self.az.shape ==  self.el.shape
+        assert self.az2pts.shape == self.el2pts.shape
+        assert self.az.ndim == 2
+    
+        npts = self.az2pts.size  #numel
+        nearRow = empty(npts,dtype=int)
+        nearCol = empty(npts,dtype=int)
+        for ipt in range(npts):
+            #we do this point by point because we need to know the closest pixel for each point
+            errdist = absolute( hypot(self.az - self.az2pts[ipt],
+                                      self.el - self.el2pts[ipt]) )
+    
+    # ********************************************
+    # THIS UNRAVEL_INDEX MUST BE ORDER = 'C'
+            nearRow[ipt],nearCol[ipt] = unravel_index(errdist.argmin(),
+                                                      (self.ypix, self.xpix),order='C')
+    #************************************************
+    
+    
+        if discardEdgepix:
+            edgeind = where(logical_or(logical_or(nearCol==0,nearCol == self.xpix-1),
+                                       logical_or(nearRow==0,nearRow == self.ypix-1)) )[0]
+            nearRow = delete(nearRow,edgeind)
+            nearCol = delete(nearCol,edgeind)
+            if self.dbglvl>0: print('deleted',edgeind.size, 'edge pixels ')
+
+        self.findLSQ(nearRow, nearCol)
+    
+        if self.dbglvl>0:
+            clr = ['b','r','g','m']
+            ax = figure().gca()
+            ax.plot(nearCol,nearRow,color=clr[int(self.name)],label='cam'+self.name+'preLSQ',
+                    linestyle='None',marker='.')
+            ax.legend()
+            ax.set_xlabel('x'); ax.set_ylabel('y')
+            #ax.set_title('pixel indices (pre-least squares)')
+            ax.set_xlim([0,self.az.shape[1]])
+            ax.set_ylim([0,self.az.shape[0]])
---
+++ main_hist.py
@@ -0,0 +1,234 @@
+#!/usr/bin/env python3
+"""
+ Michael Hirsch
+ May 2014
+ GPL v3+
+
+ Procedure:
+ 0) ingest simulation parameters from XLS file, sanity check, load data
+ 1) Make an auroral phantom (if not real data, a Simulation, and optionally for a first guess for reconstruction)
+ 2) create projection matrix
+ 3) reconstruction of VER
+ 4) fit differential number flux to reconstructed VER
+ 5) Test reconstruction & fit by reprojecting to synthetic image brightness
+
+"""
+from __future__ import division,print_function
+from signal import signal,SIGINT #for Ctrl C
+from os.path import expanduser
+from numpy import absolute,zeros,asarray,in1d,arange
+from numpy.random import normal
+import h5py
+from os.path import join
+from sys import path
+path.extend(['../python-mapping','../astrometry'])
+from sanityCheck import getParams
+
+def doSim(ParamFN,savedump,makeplot,datadump,timeInds,overrides,progms,x1d,vlim,animtime, dbglvl):
+    # local -- these were put here so that matlplotlib backend autoselect could happen first
+    from AuroraFwdModel import getSimVER
+    from transcararc import getMp,getPhi0 #calls matplotlib
+    from observeVolume import getEll,getObs #calls matplotlib
+    from FitVER import FitVERopt as FitVER #calls matplotlib
+#    from simulFrame import getSimulData
+    from plotsnew import goPlot
+    from analysehst import analyseres
+#%% Step 0) load data
+    ap,sim,cam,Fwd = getParams(ParamFN, overrides,savedump,progms,dbglvl)
+#%% setup loop
+    if sim.realdata:
+        cam,rawdata,sim = getSimulData(sim,cam,makeplot,progms,dbglvl)
+        nTimeSlice = cam['0'].keo.shape[1] #FIXME assumes equal num. of time slices list(cam.keys())[0]
+    else: #simulation
+        rawdata = None
+        if sim.raymap == 'astrometry': #and any('b' in m[:2] for m in makeplot):
+            from get1Dcut import get1Dcut #we need cam.angle_deg for plotting
+            cam = get1Dcut(cam,sim.useCamInd,makeplot,progms,dbglvl)
+        nTimeSlice = ap.shape[1]
+    timeInds = sim.maketind(timeInds,nTimeSlice)
+#%% Step 1) get projection matrix
+    Lfwd,Fwd,cam = getEll(sim,cam,Fwd,makeplot,dbglvl)
+#%% preallocation
+    jfitAll = []; Phi0All=[];  drnAll = []; bfitAll=[];  jAll= []; PfitAll = []
+#%% load flux
+    Peig = getMp(sim,Fwd['z'],makeplot,dbglvl)
+#start looping for each time slice in keogram (just once if simulated)
+
+    for ti in timeInds:
+        if not sim.realdata:
+            if ap.ix['useArc',ti] != 1:
+                print('* skipping arc ' + str(ti))
+                continue
+            #%% Step 1a) Forward model
+            Phi0 = getPhi0(sim,ap.ix[:,ti],sim.Jfwdh5,Fwd['x'],
+                           Peig['Ek'], sim.minbeamev,makeplot,dbglvl)
+            Pfwd,arc = getSimVER(Phi0, Peig, Fwd, sim, ap.ix[:,ti], ti, dbglvl)
+        else: Phi0 = None; Pfwd = None; arc = None
+#%% Step 2) Observe Forward Model (create vector of observations)
+        bn = getObs(sim,cam,Lfwd,ti,Pfwd,makeplot,dbglvl)
+        drnAll.append(bn)
+#%% Step 3) Reconstruction (not used in long time)
+        #vhat,vr0 = ReconVER(L,bn,sim,ap,cp,Fwd,ti,makeplot,dbglvl)
+#%% Step 4) fit constituent energies to our estimated vHat and reproject
+        if overrides['fwdguess'][0] == 'true':
+            Phi0r = Phi0.ravel(order='F')
+            print('****************************************************************')
+            print('** WARNING: feeding minimizer the true answer--testing only!! **')
+            print('****************************************************************')
+        elif overrides['fwdguess'][0] == 'randn':
+            randfact = absolute(normal(1,overrides['fwdguess'][1], size=Phi0.size))
+            print('** WARNING: feeding minizer true answer times ' + str(randfact))
+            Phi0r = randfact * Phi0.ravel(order='F')
+        else: #normal case, no a priori
+            Phi0r = zeros(Fwd['sx']*Peig['Mp'].shape[1],dtype=float) #ones() is NOT appropriate -- should be tapered down for higher energy beams!! per JLS!
+
+        Pfit,jfit,Tm,bfit = FitVER(Lfwd, bn, Phi0r, Peig, sim, Fwd, makeplot,dbglvl)
+#%% collect results
+        jfitAll.append(jfit); bfitAll.append(bfit)
+        Phi0All.append(Phi0)
+#%% plot results
+        goPlot(ParamFN,sim,arc,Fwd,cam,Lfwd,Tm,bn,bfit,Pfwd,Pfit,Phi0,
+                     jfit,rawdata,ti,makeplot,progms,x1d,vlim,dbglvl)
+        if animtime is not None:
+            plt.draw()
+            plt.pause(animtime)
+        elif 'show' in makeplot:
+            plt.show()
+        else:
+            plt.close('all')
+
+        if 'h5' in savedump:
+            PfitAll.append(Pfit)
+#%%
+    print('done looping')
+    analyseres(sim,Fwd['x'],Fwd['xPixCorn'],sim.useCamInd,cam,
+                   Phi0All,jfitAll,drnAll,bfitAll,vlim,makeplot,progms)
+#%% debug: save variables to MAT file
+    if 'mat' in savedump:
+        from scipy.io import savemat
+        cMatFN = ''.join((progms,'/comparePy','.mat'))
+        try:
+            print('saving to:',cMatFN)
+            vd = {'drnP':bn,'LP':Lfwd,'vP':Pfwd,'vfitP':Pfit,#'vhatP':Phat['vART'],
+                  'xPixCorn':Fwd['xPixCorn'],'zPixCorn':Fwd['zPixCorn']}
+            savemat(cMatFN,oned_as='column',mdict=vd )
+        except:
+            print('failed to save to mat file')
+    if 'h5' in savedump:
+        ch5fn = ''.join((progms,'/fitted','.h5'))
+        jAll = asarray(jAll).transpose(axes=(1,2,0)) #asarray puts pages first
+        print('saving to:', ch5fn)
+        with h5py.File(ch5fn,'w',libver='latest') as fid:
+            fid.create_dataset("/Jflux",data=jAll)
+            fid.create_dataset("/Vfit",data=PfitAll)
+
+    #return jfitAll,Lfwd
+#%% =======================================================================
+def signal_handler(signal, frame):
+    print('\n *** Aborting program as per user pressed Ctrl+C ! \n')
+    exit(0)
+#%% -----------------------------------------------------------
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+    from sys import argv
+
+    signal(SIGINT, signal_handler) #allows friendly ctrl-c interrupt
+
+    p = ArgumentParser(description='analyzes HST data and makes simulations')
+    p.add_argument('infile',help='.xls filename with simulation parameters',type=str)
+    p.add_argument('outdir',help='directory for output',type=str,nargs='?',default='')
+    p.add_argument('-d','--debug',help='set debugging verbosity',default=0,type=float)
+    p.add_argument('--mat',help='save matlab .mat file of results',action="store_true")
+    p.add_argument('--h5',help='save HDF5 .h5 file of fit results',action="store_true")
+    p.add_argument('--dump',help='dump debugging data to .h5 at key points of program',action="store_true")
+    p.add_argument('-m','--makeplot',help='list plots you want made',nargs='+',default=[''],type=str) #None gave type errors in generators & list comp.
+    p.add_argument('-p','--showplot',help='show plots on screen',action="store_true")
+    p.add_argument('-f','--frames',help='START STOP STEP of time indices',nargs=3,default=None,type=int)
+    p.add_argument('--profile',help='Profile performance of program (development/debug only)',action='store_true')
+    p.add_argument('-c','--cam',help='zero-indexed cameras to use (overrides XLS)',nargs='+',default=[None],type=int)
+    p.add_argument('--cx',help='override cam positions (must specify all)',nargs='+',default=[None],type=float)
+    p.add_argument('--influx',help='flux .h5 file to use (overrides XLS)',default=None,type=str)
+    p.add_argument('--logplot',help='logarithmic axis scaling where appropriate',action='store_true')
+    p.add_argument('--x1d',help='required location [km] of x for 1-D flux plots',default=None,type=float)
+#    p.add_argument('--saveall',help='saves "all" variables to disk, useful for registration case tracking',action='store_true')
+    p.add_argument('-a','--anim',help='animate plots (crudely)',type=float,default=None)
+    p.add_argument('--filter',help='optical filter choices: bg3   none',type=str,default=None)
+    p.add_argument('--minev',help='minimum beam energy to include in fwd model',type=float,default=None)
+    p.add_argument('--fwdguess',help='feed minimizer fwd answer. true | randn stddev |',type=str,nargs='+',default=[None])
+    p.add_argument('--fitm',help='override fit (minimization) method',type=str,default=None)
+    p.add_argument('--vlim',help='xmin xmax zmin zmax pmin pmax   limits for VER plots',type=float,nargs=6,default=(None,None,None,None,None,None))
+    p.add_argument('--jlim',help='MIN MAX flux limits for diff num flux plots',type=float,nargs=2,default=(None,None))
+    p.add_argument('--blim',help='MIN MAX flux limits for brightness plots',type=float,nargs=2,default=(None,None))
+    p.add_argument('-L','--ell',help='force recomputation of sparse matrix L',action='store_true')
+    ar = p.parse_args()
+
+    ParamFN = expanduser(ar.infile)
+    savedump=[None]
+    if ar.mat: savedump.append('mat')
+    if ar.h5: savedump.append('h5')
+#%% plot setup
+    makeplot = ar.makeplot
+    if ar.showplot: makeplot.append('show')
+    if ar.logplot: makeplot.append('log')
+    # these matplotlib imports MUST GO IN THIS ORDER
+    import matplotlib as mpl
+    #from mpl_toolkits.mplot3d import Axes3D #causes  TypeError: unhashable type: 'list'
+    if in1d(makeplot,('png','eps')).any():
+        print('** using Agg backend: Visibly displayed plots are disabled!')
+        mpl.use('Agg') #for fast PNG writing, but does NOT display at all!
+    elif in1d(makeplot,'pdf').any():
+        print('** using PDF backend: Visibly displayed plots are disabled!')
+        mpl.use('pdf') #for multipage PDF writing, but does NOT display at all!
+    else:
+        pass
+        #mpl.use('Qt4Agg') # NOT FOR ANACONDA3
+        #mpl.use('Tkagg') # possibly faster than qt4agg
+    import matplotlib.pyplot as plt
+    print(('matplotlib backend / version: ' + mpl.get_backend() +'  ' + mpl.__version__  ))
+#%%
+    vlim = {'p':ar.vlim,'j':ar.jlim,'b':ar.blim}
+#%%
+    timeInds = ar.frames
+    doProfile = ar.profile
+    datadump = ar.dump
+
+    progms = ar.outdir #'out/' + datetime.now().strftime('%Y-%m-%dT%H:%M:%S') + '/'
+    if progms:
+        with open(join(progms,'cmd.log'),'w') as f:
+            f.write(' '.join(argv))
+#%% overrides
+    overrides = {'minev': ar.minev,'filter':ar.filter,
+                 'fwdguess':ar.fwdguess, 'fitm':ar.fitm,'cam':ar.cam,
+                 'camx':ar.cx,'ell':ar.ell,'Jfwd':ar.influx}
+
+    if timeInds is not None:
+        timeInds = arange(timeInds[0],timeInds[1],timeInds[2]) #NOT range!!
+
+    if doProfile: #devel debug
+        #from subprocess import Popen, PIPE
+        import cProfile
+        from profileRunHSTsim0 import goCprofile
+        profFN = 'hstprofstats.pstats'
+        print('saving profile results to ' + profFN)
+        cProfile.run('doSim(ParamFN,savedump,makeplot,datadump,timeInds,overrides,progms,ar.x1d,vlim,ar.anim,dbglvl=ar.debug)',profFN)
+
+        #binpath = expanduser('~/profile/')
+        #sysCall = [binpath + 'gprof2dot.py','-f','pstats',profFN,'|','dot','-Tpng','-o','output.png']
+        #print(sysCall)
+        #po = Popen(sysCall, stdout=PIPE, cwd=binpath, shell=False)
+        #so,serr = po.communicate(timeout=1) #timeout is incompatible with Python 2.
+        #print(so.decode('utf8'))
+        goCprofile(profFN)
+    else: #normal
+        doSim(ParamFN,savedump,makeplot,datadump,timeInds,
+                                      overrides,progms,ar.x1d, vlim,ar.anim,dbglvl=ar.debug)
+
+#    if ar.saveall:
+#        with h5py.File(progms + 'validate.h5',libver='latest') as fid:
+#            fid.create_dataset('/fitp/residual',data=fitpAll['/optimresidual'])
+#            fid.create_dataset("/L",data=L,        compression="gzip")
+    #exit(progms)
+
+    #if saveplots and makePlots:
+   #     print('converting to TIFF')
+   #     subprocess.call(['convert','out/Jadj_t*.png','-compress','zip','out/JadjSim.tiff'], shell=False)
