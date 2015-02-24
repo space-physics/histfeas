88a5e92b9390b9c55e8d65af2f6e543546f36efc
Michael <hirsch617@gmail.com>
2015-02-23 22:00:27 -0500

    initial commit

---
+++ EllLineLength.py
@@ -0,0 +1,199 @@
+from __future__ import print_function, division
+#from numba import jit #2-3 times slower using Numba 1.5!!
+from numpy import empty,ones,ravel_multi_index,hypot,array
+from scipy.sparse import dok_matrix
+# local
+import sys
+sys.path.extend('../cv-utils')
+from lineClipping import cohensutherland
+
+'''
+ Michael Hirsch
+
+ This code is a nest of four for loops
+ We want to implement b = Lv, where
+   b is an nCam*nPixel column-major vector of observed brightness (data numbers)
+   L is an nCam*nPixel x sx*sz dimension projection matrix
+   v is an sx*sz column-major vector of auroral VER
+ by building up b,L,v in this way, the tomographic forward projection is implemented
+ in Python numpy by b = L.dot(v), which in Matlab is b = L*v
+
+ let's start by describing b,L,v.
+ We have made a mesh in the sky, let's say 41 elements from -10 to 10km with 0.5km horizontal spacing (x)
+ and say 16 elements from 90 to 400 km with 20km vertical spacing (z)
+ That means v will be a 16*41 = 656 element vector and L will have 656 columns
+ now let's say we have 2 cameras (nCam=2) each with 512 pixels in a 1-D cut of the sky through a common volume (so carefully selected).
+ then L will have 2*512 = 1024 rows, thus L is a 1024x656 matrix.
+ finally, b will be 2*512 = 1024 element vector.
+
+ next, the for loops from the inside out
+ The innermost loop (z) goes over each altitude from bottom to top (90km to 390km in our example)
+ the next innermost loop (x) goes over each horizontal bin from left to right (-10km to 10km in our example)
+ the next loop (k) is for each pixel of the particular camera
+ the outer loop (cam) is for each camera
+
+ in words, this program implements the Cohen-Sutherland line-clipping algorithm over each of the sky
+ pixels from bottom to top, left to right, for each pixel of each camera. it finds the
+ line length "ell" for each element of L. This is how we implement the line integral.
+'''
+
+def EllLineLength(Fwd,xFOVpixelEnds,zFOVpixelEnds,xCam,zCam,saveEll,Np,sim,makePlots,dbglvl):
+    plotEachRay=False
+    writeRays = False #write pixel rays to hdf5 for viewing
+
+#%% convenience variables (easier to type)
+    sx =  Fwd['sx']
+    sz =  Fwd['sz']
+    nCam = sim.nCam
+    xpc = Fwd['xPixCorn']
+    zpc = Fwd['zPixCorn']
+    maxNell = Fwd['maxNell']
+    useCamInd = sim.useCamInd
+    assert xpc.size == sx + 1
+    assert zpc.size == sz + 1
+#%% preallocation
+#This goes OUTSIDE all loops!
+    if dbglvl>0:
+        print('EllLineLength: Number of 1D pixels in cut: ',Np)
+        print('x: ', end=''); print(Fwd['x'])
+        print('xpc: ',end=''); print(xpc)
+        print('z: ', end=''); print(Fwd['z'])
+        print('zpc: ',end=''); print(zpc)
+
+    L,xzplot = goCalcEll(maxNell,nCam,Np,sz,sx,xpc,zpc,xFOVpixelEnds,zFOVpixelEnds,
+                          xCam,zCam,useCamInd,plotEachRay,dbglvl)
+
+    #%% write results to HDF5 file
+    if saveEll:
+        doSaveEll(L,Fwd,sim.FwdLfn,xFOVpixelEnds,zFOVpixelEnds,xCam,zCam,writeRays)
+
+    if 'ell' in makePlots and plotEachRay:
+        plotEll(sim.useCamInd,xFOVpixelEnds,zFOVpixelEnds,xCam,zCam,Np,xpc,zpc,
+                sz,sx,xzplot,sim.FwdLfn,plotEachRay,makePlots,
+                (None,None,None,None,None,None))
+
+    return L
+
+#@jit #slower using Numba 1.5
+def goCalcEll(maxNell,nCam,Np,sz,sx,xpc,zpc,xFOVpixelEnds,zFOVpixelEnds,xCam,zCam,useCamInd,plotEachRay,dbglvl=0):
+    #FIXME assumes all cameras have same # of pixels
+    #Lrows = empty( ( maxNell * nCam * Np ) , dtype=int, order='F')
+    #Lcols = empty( ( maxNell * nCam * Np ) , dtype=int, order='F') #empty_like(Lrows)
+    #Lvals = empty( ( maxNell * nCam * Np ) , dtype=float, order='F') #empty_like
+#    LindStart = 0 #different than Matlab
+ #   LindEnd = 0 #in case none found before k%100 first print to screen
+    inttot = 0
+    #L = zeros( ( Np*nCam,sz*sx),dtype=float ,order='F')
+    L = dok_matrix(( Np*nCam,sz*sx),dtype=float)
+    print('Dimensions of L:',L.shape,' sz=',sz, '  sx=',sx )
+
+    Lcol =   empty(maxNell, dtype=int) #we'll truncate this to actual length at end
+    tmpEll = empty(maxNell, dtype=float) #we'll truncate this to actual length at end
+
+    xzplot = [] #we'll append to this
+
+#%%let's compute intersections!!
+    ''' We MUST have all cameras enabled for this computation (as verified in observeVolume)'''
+#    try:
+    for iCam in useCamInd:
+        xfov = xFOVpixelEnds[:,iCam]; zfov = zFOVpixelEnds[:,iCam]
+        for k in range(Np): #FIXME assumes all cam same 1D cut pixel length
+            nHitsThisPixelRay = 0
+            for xInd in range(sx):
+                for zInd in range(sz):
+                    '''get the vertices of the intersection between voxel and pixel
+                    for the kth pixel FOV, find the intersection with each sky polygon
+                     '''
+                    x1, y1, x2, y2 = cohensutherland(
+                             xpc[xInd], zpc[zInd + 1],
+                              xpc[xInd + 1], zpc[zInd],
+                              xCam[iCam], zCam[iCam],
+                              xfov[k], zfov[k])
+                    if x1 is not None:
+                        #assert nHitsThisPixelRay <= maxNell #removed for performance
+                        Lcol[nHitsThisPixelRay] = ravel_multi_index((zInd,xInd),dims=(sz,sx),order='F')
+                        tmpEll[nHitsThisPixelRay] = hypot(x2-x1,y2-y1)
+                        nHitsThisPixelRay += 1 
+                        if plotEachRay:
+                            xzplot.append(array([x1,x2,y1,y2]))
+            if nHitsThisPixelRay > 0: #this is under "for k" level
+                inttot += nHitsThisPixelRay
+                L[iCam * Np + k, Lcol[:nHitsThisPixelRay]] = tmpEll[:nHitsThisPixelRay]
+            if k%100 == 0: #arbitrary display update interval #this is under "for k" level
+                print('Camera #',iCam,': {:0.0f}'.format(1.0*k/Np*100)
+                      +'% complete, found',inttot, 'intersections.')
+
+#    except IndexError:
+#        exit('**** ERROR on iCam='+str(iCam)+', k=' +str(k)+', xInd='+str(xInd)+', zInd='+str(zInd)+' *****')
+
+    print('Total number of intersections found:',inttot)
+
+    return L.tocsc(), xzplot
+
+def doSaveEll(L,Fwd,EllFN,xFOVpixelEnds,zFOVpixelEnds,xCam,zCam,writeRays):
+    import h5py
+    print('writing', EllFN )
+    with h5py.File(EllFN,'w',libver='latest') as fid:
+        h5L = fid.create_dataset("/L",data=L.todense(),compression="gzip");  h5L.attrs['Units'] = 'kilometers'
+        h5Fwdx = fid.create_dataset("/Fwd/x",data=Fwd['x']); h5Fwdx.attrs['Units'] = 'kilometers'
+        h5Fwdz = fid.create_dataset("/Fwd/z",data=Fwd['z']); h5Fwdz.attrs['Units'] = 'kilometers'
+        h5FwdxPC = fid.create_dataset("/Fwd/xPixCorn",data=Fwd['xPixCorn']); h5FwdxPC.attrs['Units'] = 'kilometers'
+        h5FwdzPC = fid.create_dataset("/Fwd/zPixCorn",data=Fwd['zPixCorn']); h5FwdzPC.attrs['Units'] = 'kilometers'
+
+        #h5ObsPA =  fid.create_dataset("/Obs/pixAngle",data=pixAngleDeg);  h5ObsPA.attrs['Units'] = 'Degrees'
+        h5ObsxFPE = fid.create_dataset("/Obs/xFOVpixelEnds",data=xFOVpixelEnds); h5ObsxFPE.attrs['Units'] = 'kilometers'
+        h5ObszFPE = fid.create_dataset("/Obs/zFOVpixelEnds",data=zFOVpixelEnds); h5ObszFPE.attrs['Units'] = 'kilometers'
+        h5xCam = fid.create_dataset('/Obs/xCam',data=xCam); h5xCam.attrs['Units'] = 'kilometers'
+        h5zCam = fid.create_dataset('/Obs/zCam',data=zCam); h5zCam.attrs['Units'] = 'kilometers'
+
+def plotEll(useCamInd,xFOVpixelEnds,zFOVpixelEnds,xCam,zCam,Np,xpc,zpc,sz,sx,
+            xzplot,EllFN,plotEachRay,makeplot,vlim):
+    from matplotlib.pyplot import figure, draw, pause
+    from matplotlib.ticker import MultipleLocator
+    decimfactor = 8
+    clrs = ['r','g','y','m']
+    afs = 20
+    tkfs = 20
+    tfs = 22
+
+    fg = figure()
+    ax = fg.gca()
+    #this pcolormesh shows the model grid, with a white background per JLS
+    ax.pcolormesh(xpc,zpc,ones((sz,sx)), cmap='bone', vmin= 0, vmax=1,
+                  edgecolor='k', linewidth=0.001)
+
+
+    if plotEachRay:
+        print('plotting viewing geometry')
+        for ixz in range(len(xzplot)):
+            ax.plot(x=xzplot[ixz][:2],
+                    y=xzplot[ixz][-2:],
+                    color='red')
+            draw() #need plt.pause(0.01) just after this
+            pause(0.01) #need plt.draw() just before this
+    else:
+        for iCam in useCamInd:
+            for iray in range(0,Np,decimfactor):
+                #DO NOT SPECIFY x=... y=... or NO LINES appear!!
+                ax.plot([xCam[iCam],xFOVpixelEnds[iray,iCam]],
+                        [zCam[iCam],zFOVpixelEnds[iray,iCam]],
+                        color=clrs[iCam])
+    if vlim[0] is None:
+        ax.set_xlim((xpc[0], xpc[-1]))
+        ax.set_ylim((0,      zpc[-1]))
+    else:
+        ax.set_xlim(vlim[:2])
+        ax.set_ylim(vlim[2:4])
+    ax.set_xlabel('$B_\perp$ [km]', fontsize = afs)
+    ax.set_ylabel('$B_\parallel$ [km]', fontsize = afs)
+    ax.set_title('Viewing geometry for $L$', fontsize = tfs)# + EllFN)
+
+    ax.yaxis.set_major_locator(MultipleLocator(100))
+    ax.yaxis.set_minor_locator(MultipleLocator(20))
+    ax.xaxis.set_major_locator(MultipleLocator(1))
+    ax.xaxis.set_minor_locator(MultipleLocator(0.2))
+    ax.tick_params(axis='both', which='both', labelsize=tkfs, direction='out')
+    if 'png' in makeplot:
+        figpng = EllFN + '.png'
+        print('saving', figpng)
+        fg.savefig(figpng,bbox_inches='tight',dpi=600)  # this is slow and async
