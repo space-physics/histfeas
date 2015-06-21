da88b194f3c53dd9afca2c25ca4bc14e17d5f860
Michael <scienceopen@users.noreply.github.com>
2015-06-20 22:52:49 -0700

    overall offline analysis

---
+++ loadAnalyze.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+from __future__ import division,absolute_import
+import h5py
+from os.path import join,expanduser,splitext
+from numpy import asarray
+from warnings import warn
+#
+from analysehst import analyseres
+from sanityCheck import getParams
+from plotsnew import plotB
+
+vlim={'b':(None,None)}
+
+def runtest(h5list,xlsfn,overrides,makeplot,verbose=0):
+    Phifwd =[]; Phidict =[]; dhat=[]; drn=[]
+    
+    nt = len(h5list)
+    
+    for h5 in h5list:
+        with h5py.File(h5,'r',libver='latest') as f:
+            x  = f['/pfwd/x'].value #same for all in directory
+            xp = f['/pfwd/xp'].value
+            
+            Phifwd.append(f['/phifwd/phi'].value)
+            Phidict.append({'x':f['/phiest/phi'].value, 
+                            'EK':f['/phiest/Ek'].value})
+            try:
+                dhat.append(['/best/bfit'].value)
+                drn.append(f['/best/braw'].value)
+            except Exception:
+                pass
+#%%  
+    stem,ext = splitext(h5)  #all in same directory
+    
+    Phifwd = asarray(Phifwd).transpose(1,2,0)
+        
+    analyseres(None,None,
+               x, xp, Phifwd, Phidict, drn, dhat,
+               vlim=vlim, makeplot=[None],
+               progms=ext, verbose=verbose)
+
+#%%
+    if xlsfn is None: 
+        warn('No XLSX parameter file found')
+        return
+        
+    ap,sim,cam,Fwd = getParams(xlsfn,overrides,makeplot,progms=None,verbose=verbose)
+ 
+    for ti in range(nt):
+        plotB(drn[ti],sim.realdata,cam,vlim['b'],ti,makeplot,'$br',None,verbose)
+    
+    
+if __name__ == '__main__':
+    from glob import glob    
+    #
+    from argparse import ArgumentParser
+    p = ArgumentParser(description="load HiST output and plot/analyse")
+    p.add_argument('h5path',help='path containing dump.h5 outputs (Hist output)')
+    p = p.parse_args()
+    
+    h5list = glob(expanduser(join(p.h5path,'dump_*.h5')))
+    h5list.sort()
+    
+    xlsfn = glob(expanduser(join(p.h5path,'*.xlsx')))
+    if len(xlsfn) == 0: xlsfn=[None]
+    
+    runtest(h5list,xlsfn[0],overrides=None)
