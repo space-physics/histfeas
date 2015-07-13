b2a3bbfad3cce4f4754fcd5b31ceef7cec98f0e5
michael <hirsch617@gmail.com>
2015-07-13 01:39:09 -0400

    don't double-scale actual data

--- camclass.py
+++ camclass.py
@@ -90,7 +90,7 @@ class Cam: #use this like an advanced version of Matlab struct
         self.pedn = cp['pedn']
         self.ampgain = cp['ampgain']
 
-        if isfinite(self.kineticSec) and isfinite(self.pixarea_sqcm) and isfinite(self.pedn):
+        if not sim.realdata and (isfinite(self.kineticSec) and isfinite(self.pixarea_sqcm) and isfinite(self.pedn)):
             self.intens2dn = self.kineticSec * self.pixarea_sqcm * self.ampgain / self.pedn
         else:
             self.intens2dn = 1
