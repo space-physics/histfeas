4dbfb92f0d15d1a52b683bee7f0bd67c76e00053
scivision <scivision@users.noreply.github.com>
2015-08-06 01:29:07 -0400

    selftest output dir

--- test/registration.py
+++ test/registration.py
@@ -18,7 +18,7 @@ def hist_registration(regh5,regXLS):
                   makeplot=['fwd','optim'],
                   timeInds=None,
                   overrides = None, #{'minev': minev,'filter':filt, 'fwdguess':fwdguess, 'fitm':fitm,'cam':cam,'camx':acx,'ell':ell,'Jfwd':influx},
-                  progms = None,
+                  progms = 'out',
                   x1d=[None],
                   vlim = {'p':[None]*6,'j':[None]*2,'b':[None]*2},
                   animtime=None,
