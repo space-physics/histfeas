6a73faafd87da1436ccd9ead28f8abd62a44ceb4
Michael <scienceopen@users.noreply.github.com>
2015-08-29 23:50:18 -0400

    fix imports

--- test/registration.py
+++ test/registration.py
@@ -3,6 +3,7 @@
 Registration case for HiST program
 Michael Hirsch
 """
+from __future__ import division,absolute_import
 from numpy import isclose
 from numpy.testing import assert_allclose
 from sys import argv
@@ -47,7 +48,8 @@ def writeout(regh5):
         f['/phifwd/x0'] = 1.
 
 if __name__ == '__main__':
-    regh5='test/registration.h5';     regXLS='test/registration.xlsx'
+    regh5='test/registration.h5'
+    regXLS='test/registration.xlsx'
 
     Phi0,Phifit=hist_registration(regh5,regXLS)
     readCheck(Phi0,Phifit)
