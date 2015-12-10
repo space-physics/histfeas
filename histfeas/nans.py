3ae3d67f887639c9ce2b7db514000d5cbdf05316
scivision <scivision@users.noreply.github.com>
2015-12-10 14:21:07 -0800

    timestamp subplots

--- histfeas/nans.py
+++ histfeas/nans.py
@@ -4,6 +4,7 @@ test results show this is ~5 times faster than nan*empty()
 GPLv3+
 """
 from numpy import empty,nan
+
 def nans(shape=1, dtype=float, order='F'):
     a = empty(shape, dtype, order)
     a.fill(nan)
