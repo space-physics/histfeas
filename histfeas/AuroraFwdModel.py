2a971a4760a08d998f45fd865b7218b4ac43f5b7
michael <scienceopen@noreply.github.com>
2016-04-19 10:05:21 -0400

    allow specifying impulse and flat Phi_top precipitation

--- histfeas/AuroraFwdModel.py
+++ histfeas/AuroraFwdModel.py
@@ -19,13 +19,14 @@ def getSimVER(Phi0,Mp,Fwd,sim,ap,tInd):
     Pfwd=zeros((Fwd['z'].size,Phi0.shape[1]),float) #TODO for non transcar cases
     for a in ap:
         #verify that all have same shape in B_\parallel
-        assert (ap[a].loc['Zshape',0] == ap[a].loc['Zshape',:]).all()
-        if ap[a].at['Zshape',0] == 'transcar':
+        assert (ap[a].loc['Zshape',0] == ap[a].loc['Zshape',:]).all(),'upgrade code to meld Zshapes'
+
+        if ap[a].at['Zshape',0] in ('flat','impulse','transcar'):
             '''
             recall that phi(z,E) is 2-D matrix, and other matrices involved
             must be updated accordingly from original April 2014 try code
             '''
-            #WARNING: The line below IS ****NOT**** += !
+            #NOTE: The line below IS **NOT** += !
             return getColumnVER(sim.useztranscar, Mp['ztc'], Mp['Mp'], Phi0)
         else:
             warn('these cases not yet updated for upsampled time!')
