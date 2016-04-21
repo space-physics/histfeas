202fbe0cdd08a735948a09f331609388f360679b
Michael <scienceopen@users.noreply.github.com>
2016-04-20 23:07:08 -0400

    script to run camera spacing

--- campos_impulse.sh
+++ campos_impulse.sh
@@ -6,7 +6,7 @@ cam1=(0.5 1 1.5 2 2.5 3 3.5 4 5 6 7 8 9 10)
 cam0=0
 
 for c in ${cam1[*]}; do
-	./figure_impulse --cx $cam0 $c -o ~/Dropbox/HirschThesis/impulse/cam1_$c
+	./figure_impulse.py --cx $cam0 $c -o ~/Dropbox/HirschThesis/impulse/cam1_$c
 done
 
 
