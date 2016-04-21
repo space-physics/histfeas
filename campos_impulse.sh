00e8281a8471fb4067f0f4378b3b2bae9720ef2c
scivision <scivision@users.noreply.github.com>
2016-04-21 02:15:40 -0400

    titling, labeling, directory names

--- campos_impulse.sh
+++ campos_impulse.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-
+set -u; set -e
 # iterates over camera positions, using the step/impulse DNF input.
 
 cam1=(0.5 1 1.5 2 2.5 3 3.5 4 5 6 7 8 9 10)
 cam0=0
 
 for c in ${cam1[*]}; do
-	./figure_impulse.py --cx $cam0 $c -o ~/Dropbox/HirschThesis/impulse/cam1_$c
+    cx=$(printf "%0.2f" $c)
+	./figure_impulse.py --cx $cam0 $cx -o ~/Dropbox/HirschThesis/impulse2/cam1_$cx
 done
 
 
