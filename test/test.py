ee54a8617ab9df7e86a7f55a892d0e40d0c6ea73
Michael <scienceopen@users.noreply.github.com>
2016-04-21 17:20:41 -0400

    platform indp. rootdir for selftest

--- test/test.py
+++ test/test.py
@@ -17,13 +17,14 @@ matplotlib.use('Agg') #fixes Travis NO DISPLAY bug
 #
 from histfeas.main_hist import doSim
 
-
+rootdir = Path(__file__).parents[1]
+print(rootdir)
 
 def hist_registration(regh5,regXLS):
     Phi0,Phifit =doSim(ParamFN=regXLS,
                   makeplot=['fwd','optim'],
                   timeInds=None,
-                  overrides = {'ell':True,'rootdir':'~/code/histfeas'}, #{'minev': minev,'filter':filt, 'fwdguess':fwdguess, 'fitm':fitm,'cam':cam,'camx':acx,'ell':ell,'Jfwd':influx},
+                  overrides = {'ell':True,'rootdir':rootdir}, #{'minev': minev,'filter':filt, 'fwdguess':fwdguess, 'fitm':fitm,'cam':cam,'camx':acx,'ell':ell,'Jfwd':influx},
                   odir = gettempdir(),
                   x1d=[None],
                   vlim = {'p':[None]*6,'j':[None]*2,'b':[None]*2},
