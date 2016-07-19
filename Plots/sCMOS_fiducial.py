c55c887f341d4e68ebad2016b1b87632bec860d3
scivision <scivision@users.noreply.github.com>
2016-07-19 00:46:43 -0400

    user friendly time limit spec

--- Plots/sCMOS_fiducial.py
+++ Plots/sCMOS_fiducial.py
@@ -3,18 +3,29 @@
 creates sCMOS fiducials for TGARS 2015 Reconstruction of fine scale auroral Dyanmics paper figure
 The AVI reader accounts for the full annotated Matlab axes frame and indexes accordingly
 """
-from numpy import arange
+from numpy import arange,datetime64,timedelta64
 import cv2
 from matplotlib.pyplot import show
 #
+from cvutils.getaviprop import getaviprop
 from histfeas import Path
 from histfeas.fiducial import fiducial
 #%% sCMOS Figure
 path = '~/data/2011-03-01/optical'
 vidfn= 'CMOS_110301_1006.avi'
-flist = range(100,102) # frame indices
-
 cadence_sec = 0.02 # by inspection of video title
+t0 = datetime64('2011-03-01T10:06:09.06')
+dt = timedelta64(int(cadence_sec*1e3),'ms')
+
+tlim = (datetime64('2011-03-01T10:06:06'),
+        datetime64('2011-03-01T10:06:22'))
+#%%
+fn = Path(path).expanduser() / vidfn
+finf = getaviprop(fn)
+
+t = arange(t0, finf['nframe']*dt, dt)
+
+tlist = t[(t>tlim[0]) & (t<tlim[1])]
 
 xycrop=(0,0) # normally (0,0) for uncropped original video
 #%% zero-based indexing from upper left of image, magnetic zenith
@@ -30,22 +41,20 @@ ringmult = arange(2,12,2,dtype=float) # magnetic zenith angle to label with ring
 rings=True
 rays=False
 
-path = Path(path).expanduser()
+vid = cv2.VideoCapture(str(fn))
 
-vid = cv2.VideoCapture(str(path/vidfn))
-
-for f in flist:
-    vid.set(cv2.CAP_PROP_POS_FRAMES,f) # advance to requested frame number 0-based
-    fact = int(vid.get(cv2.CAP_PROP_POS_FRAMES))
-    assert fact == f,'got frame {} but requested frame {}'.format(fact,f)
+for i,T in enumerate(tlist):
+    vid.set(cv2.CAP_PROP_POS_FRAMES, i) # advance to requested frame number 0-based
+    iact = int(vid.get(cv2.CAP_PROP_POS_FRAMES))
+    assert iact == i,'got frame {} but requested frame {}'.format(iact,i)
 
     ret,img = vid.read()
 #%% time label -- elapsed time since first frame assuming uniform frame rate
-    tstr = '{:.0f} ms'.format( (f-flist[0]) * cadence_sec * 1e3)
+    tstr = str(T-tlist[0])[:-12] + 'ms'
 #%% plot!
-    outfn = 'anno_{}.png'.format(f)
+    outfn = 'anno_{}.png'.format(T)
     fiducial(img, xycrop[0], xycrop[1], outfn, rings, rays,
-             tstr, magzenith_xy, wh0, ringmult, axlim)
+             T,tstr, magzenith_xy, wh0, ringmult, axlim)
 
     rings=rays=False
 
