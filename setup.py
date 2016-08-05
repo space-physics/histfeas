0a9ac38ae777bd534f7870bc087c2d3a4e074376
scivision <scivision@users.noreply.github.com>
2016-08-05 05:51:50 -0400

    prereq

--- setup.py
+++ setup.py
@@ -1,21 +1,15 @@
 #!/usr/bin/env python
-import os,sys,subprocess
+import subprocess
 from setuptools import setup
 
-exepath = os.path.dirname(sys.executable)
 try:
-    subprocess.call([os.path.join(exepath,'conda'),'install','--yes','--file','requirements.txt'])
+    subprocess.call(['conda','install','--yes','--file','requirements.txt'])
 except Exception as e:
-    print('tried conda in {}, but you will need to install packages in requirements.txt  {}'.format(exepath,e))
-
-
-with open('README.rst','r') as f:
-	long_description = f.read()
+    pass
 
 setup(name='histfeas',
       packages=['histfeas'],
 	  description='Feasibility study for HiST auroral tomography system',
-	  long_description=long_description,
 	  author='Michael Hirsch',
 	  url='https://github.com/scienceopen/histfeas',
 	  install_requires=['themisasi','histutils','lowtran','pymap3d','astrometry_azel',
