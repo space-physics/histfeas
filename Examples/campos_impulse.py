14b0c63a92562bed84d168e8136be9fd471a35d1
scivision <scivision@users.noreply.github.com>
2016-09-19 16:24:41 -0400

    two-line parallel max-worker execution architecture

--- Examples/campos_impulse.py
+++ Examples/campos_impulse.py
@@ -2,18 +2,32 @@
 """
 iterates over camera positions, using the step/impulse differential number flux input.
 """
-from histfeas import Path
+import multiprocessing
+import concurrent.futures
 import subprocess
+from histfeas import Path
 
 rdir = 'out'
 ini = '../in/2cam_split.ini'
-
-rdir = Path(rdir).expanduser()
 cam1 = (0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 5, 6, 7, 8, 9, 10) # km
 cam0 = 0 # km
+#%%
+Ncpu=multiprocessing.cpu_count()//2  # //2 makes one thread per CPU for 2 thread Intel Hyperthreading
+rdir = Path(rdir).expanduser()
 
-for cx1 in cam1:
-    cmd = ['../FigureMaker.py',ini,str(rdir/'cam1_{}'.format(cx1)),
-                      '--cx',str(cam0),str(cx1), ]
+print('using {} CPU'.format(Ncpu))
+
+def runhist(cx1):
+    cmd = ['../FigureMaker.py', ini, str(rdir/'cam1_{}'.format(cx1)),
+                      '--cx',str(cam0),str(cx1)]
     print(' '.join(cmd))
-    subprocess.Popen(cmd)
+    subprocess.run(cmd)
+
+
+def main():
+    with concurrent.futures.ProcessPoolExecutor(max_workers=Ncpu) as executor:
+        executor.map(runhist, cam1)
+
+
+if __name__ == '__main__':
+    main()
