e3a007e228d14e7d5b0e80ee56325aaecd3c07a4
scienceopen <scienceopen@users.noreply.github.com>
2016-11-19 23:19:32 -0500

    better develop setup for all prereqs

--- setup.py
+++ setup.py
@@ -1,36 +1,45 @@
 #!/usr/bin/env python
 import subprocess
 from setuptools import setup
+from os import chdir
 
 try:
     subprocess.call(['conda','install','--yes','--file','requirements.txt'])
 except Exception as e:
     pass
+#%%
+# FIXME: trick is to have them in order from no prereq to full prereq
+for p in ['https://github.com/scienceopen/themisasi',
+          'https://github.com/scienceopen/histutils',
+        'https://github.com/scienceopen/lowtran',
+        'https://github.com/scienceopen/pymap3d',
+        'https://github.com/scienceopen/astrometry_azel',
+        'https://github.com/scienceopen/CVutils',
+        'https://github.com/scienceopen/gridaurora',
+        'https://github.com/scienceopen/transcarread',
+        'https://github.com/scienceopen/gaussfitter',
+        'https://github.com/scienceopen/pyimagevideo',
+        'https://github.com/scienceopen/dascutils',
+        'https://github.com/scienceopen/pybashutils']:
 
+    chdir('..')
+    p.rstrip('/')
+    subprocess.call(['git','clone', p])
+
+    chdir(p.split('/')[-1])
+    subprocess.call(['git','pull']) #in case it was already installed
+
+    subprocess.call(['python','setup.py','develop']) #FIXME is there an API to do this?
+
+chdir('../histfeas')
+#%%
 setup(name='histfeas',
       packages=['histfeas'],
 	  description='Feasibility study for HiST auroral tomography system',
 	  author='Michael Hirsch',
 	  url='https://github.com/scienceopen/histfeas',
-	  install_requires=['themisasi','histutils','lowtran','pymap3d','astrometry_azel',
-			'cvutils','gridaurora','transcarread','pyimagevideo','dascutils','pythonutils',
-                        'gaussfitter',
-                        'Wand','pathvalidate',
-		         'geopy','simplekml'],
+	  install_requires=['Wand','pathvalidate','geopy','simplekml'],
 	  extras_require = {'tifffile':['tifffile']},
-      dependency_links = [
-        'https://github.com/scienceopen/themisasi/tarball/master#egg=themisasi',
-        'https://github.com/scienceopen/histutils/tarball/master#egg=histutils',
-        'https://github.com/scienceopen/lowtran/tarball/master#egg=lowtran',
-        'https://github.com/scienceopen/pymap3d/tarball/master#egg=pymap3d',
-        'https://github.com/scienceopen/astrometry_azel/tarball/master#egg=astrometry_azel',
-        'https://github.com/scienceopen/CVutils/tarball/master#egg=CVutils',
-        'https://github.com/scienceopen/gridaurora/tarball/master#egg=gridaurora',
-        'https://github.com/scienceopen/transcarread/tarball/master#egg=transcarread',
-        'https://github.com/scienceopen/gaussfitter/tarball/master#egg=gaussfitter',
-        'https://github.com/scienceopen/pyimagevideo/tarball/master#egg=pyimagevideo',
-        'https://github.com/scienceopen/dascutils/tarball/master#egg=dascutils',
-        'https://github.com/scienceopen/pybashutils/tarball/master#egg=pythonutils'
-                          ],
+      dependency_links = [ ],
 	  )
 
