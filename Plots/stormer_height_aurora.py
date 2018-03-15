eaeb7c1e10b98ff656e6d3ebd89a1a40e3a556a3
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-03-15 13:25:23 -0400

    cleanup, doc [ci skip]

--- Plots/stormer_height_aurora.py
+++ Plots/stormer_height_aurora.py
@@ -2,65 +2,62 @@
 """
 Stormer's trigometric auroral height determination
 Egeland 2013 p. 98
+
+Explores effects of features at different heights vs. observer standoff distance.
+Shows difficulty of fine-scale auroral observation:
+    1. need close spaced caemras to unambiguously estimate fine scale (Semeter 2012)
+    2. close cameras present difficult but solvable inversion problem (Hirsch 2015, 2017)
 """
-from sympy import sin,solve,Symbol
 import numpy as np
-from matplotlib.pyplot import figure,subplots,show
+from matplotlib.pyplot import figure,show
 import seaborn as sns
-sns.set_context('talk',font_scale=1.5)
+sns.set_context('talk',font_scale=1)
 sns.set_style('whitegrid')
 sns.set_palette('cubehelix')
 
-b = 88
-a = np.arange(45,b-.5,0.01) #degress
+b = 88 # angle to feature from observer #2
+a = np.arange(45,b-.5,0.01) #degrees  # angle to feature from observer #1
 
+d = np.array([3.4,4.3,20,50]) # observer standoff distance [km]
 
-d = np.array([3.4,4.3,20,50])
-
-b = np.radians(b); a = np.radians(a)
-h = np.sin(a) * np.sin(b) / np.sin(b-a) * d[:,None]
+br = np.radians(b); ar= np.radians(a)
+h = np.sin(ar) * np.sin(br) / np.sin(br-ar) * d[:,None]
 h[(75>h) | (h>300)] = np.nan
 #%%
 
-fg=figure(1); fg.clf()
-ax = fg.gca()
-ax.plot(np.degrees(a),h.T)
-#ax.set_ylim((75,500))
-ax.set_ylabel('altitude [km]')
-ax.set_xlabel(r'observer angle $\alpha$ [deg]')
-
-
-fg=figure(2); fg.clf()
-fg,axs = subplots(2,1,num=2,sharex=True)
+fg=figure(); fg.clf()
+axs = fg.subplots(2,1,sharex=True)
 ax=axs[0]
-ax.plot(np.degrees(a),h.T)
-ax.set_ylim((70,310))
-ax.set_xlim((60,None))
+ax.plot(a,h.T)
 ax.set_ylabel('altitude [km]')
-ax.set_title('Størmer trigonometric altitude estimation')
+ax.set_title('Størmer trigonometric altitude vs. observer standoff distance [km]')
 ax.legend(d.astype(str),loc='best')
 
 dh = np.diff(h)
 ax=axs[1]
-ax.plot(np.degrees(a[:-1]),dh.T)
+ax.plot(a[:-1], dh.T)
 #ax.set_yscale('log')
 #ax.set_ylim((0,100))
-ax.set_ylabel(r'd$h$/d$\alpha$')
-ax.set_title('Angular sensitivity vs. separation distance [km]')
+ax.set_ylabel(r'd$h$/d$\alpha$  [km/degree]')
+ax.set_title('Angular sensitivity vs. observer standoff distance [km]')
 ax.legend(d.astype(str),loc='best')
 ax.set_xlabel(r'observer angle $\alpha$ [deg]')
 show()
 
 #%% symbolic
-if 0:
+# Broken? Doesn't solve.
+if False:
+    from sympy import sin,solve,Symbol
     a = Symbol("a",real=True,positive=True)
     b = Symbol("b",real=True,positive=True)
 
     h=100
-    
+
     S=solve(sin(a) * sin(b) / sin(b-a) * d - h,(a,b) )
     print(S)
-    
-#%% reaction time
+
+#%% Human auditory reaction time
+# I neglected to write down which article these came from
+# was thinking about error due to human shutter reaction time to aurora/telephone stimulus
 rt_ms = np.array([75,85,85,85,85,85,95,95])
-print('reaction time std() {:.1f} [ms]  mean: {:.1f}'.format(rt_ms.std(),rt_ms.mean()))
+print('Human auditory reaction time std() {:.1f} [ms]  mean: {:.1f}'.format(rt_ms.std(),rt_ms.mean()))
